fe142f065ecb9ed94044f87f53f2b74bccbf9b84
Andreea <andreea.draniceanu@gmail.com>
2020-08-15 10:40:36 +0300

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinAppDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinAppDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("72f57fd9-04e5-497b-88a5-5bca4dd88b11")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace WinAppDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static WindowsDriver<WindowsElement> session;
+
+        [ClassInitialize]
+        public void BeforeTests()
+        {
+            var options = new AppiumOptions();
+            options.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+            session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+        }
+        
+
+        [TestMethod]
+        public void AddNumbers()
+        {
+            session.FindElementByAccessibilityId("num8Button").Click();
+            session.FindElementByAccessibilityId("plusButton").Click();
+            session.FindElementByAccessibilityId("num3Button").Click();
+            session.FindElementByName("Equals").Click();
+            Console.WriteLine(session.FindElementByAccessibilityId("CalculatorResults").Text);
+            Assert.IsTrue(session.FindElementByAccessibilityId("CalculatorResults").Text.Equals("Display is 11"));
+        }
+
+        [ClassCleanup]
+        public void CleanUp()
+        {
+            if (session != null) 
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+    }
+}
